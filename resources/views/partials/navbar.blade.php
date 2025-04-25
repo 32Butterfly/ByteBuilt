b6fa7360668626978d30cb8f73516dd753072fb9
Evaldas Dmitrišin <81704689+32Butterfly@users.noreply.github.com>
2025-04-25 18:14:02 +0300

    Add default profile picture
    Further addition of uploading custom photos in the future

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,12 +19,17 @@
           @endif
       @endauth
     </div>
-
     <div class="navbar-end">
       <div class="navbar-item">
         @auth
+          @if(auth()->user()->profile_picture)
+            <!--- not working yet will maybe do in the future  <img src="{{ asset('storage/' . auth()->user()->profile_picture) }}" alt="Profile Picture" class="profile-pic"> -->
+          @else
+            <img src="{{ asset('images/default_avatar.png') }}" alt="Default Avatar" class="is-rounded" style="width: 36px; height: 36px; object-fit: cover;">
+          @endif
+
           <!-- Display the user's name if logged in -->
-          <span class="navbar-item">Welcome, {{ auth()->user()->name }}</span>
+          <span class="navbar-item has-text-weight-medium">Welcome, {{ auth()->user()->name }}</span>
           <a href="{{ route('logout') }}" class="button is-light">Log out</a>
         @else
           <!-- Show the Sign Up and Log In buttons if not logged in -->
